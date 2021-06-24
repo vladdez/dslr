ab2b0e631b3009a27faae2fd622c6fe6ef1cbebb
vladdez <veehimrimidal@gmail.com>
2021-06-24 21:06:35 +0300

    g

--- pair_plot.py
+++ pair_plot.py
@@ -1,19 +1,13 @@
 from tools import csv_load
 import matplotlib.pyplot as plt
-import pandas as pd
-import argparse
 import seaborn as sns
 
 
 def pair_plot(data):
-    sns.pairplot(df, hue='class')  
+    sns.set(style="ticks", color_codes=True)
+    g = sns.pairplot(data)
     plt.show()
 
 if __name__ == '__main__':
-    data  = csv_load('datasets/dataset_train.csv')
-    parser = argparse.ArgumentParser()
-    #parser.add_argument("--subject1", default = "Astronomy")
-    #parser.add_argument("--subject2", default = "Defense Against the Dark Arts")
-    #args = parser.parse_args()
-    #scatter_plot(data, args.__dict__['subject1'], args.__dict__['subject2'])
+    data = csv_load('datasets/dataset_train.csv')
     pair_plot(data.loc[:, 'Arithmancy':'Flying'])
